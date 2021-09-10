abd7318b82a05aaa092bb1b61f8a48584fa08c9f
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2021-09-10 22:29:55 +0530

    first commit

---
+++ github.js
@@ -0,0 +1,23 @@
+class Github {
+  constructor() {
+    this.client_id = 'cf006b5dd7a6232a2e4d';
+    this.client_secret = ' b9d657b2d2904d037f898f55c9ae7be36561e125';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+
+  }
+}
